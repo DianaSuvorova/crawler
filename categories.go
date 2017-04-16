d37d7496a18781ea5387e0b98a99a91eefade36e
Diana Suvorova <diana.suvorova@ayasdi.com>
2017-04-16 14:00:00 -0700

    reading connection from args

--- categories.go
+++ categories.go
@@ -6,7 +6,6 @@ import (
   "regexp"
   "strings"
   "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 type categoryPage struct {
@@ -24,13 +23,13 @@ func NewCategoryPage(url string) *categoryPage {
   cp := new(categoryPage)
   cp.category = new(category)
   cp.category.url = url
-  cp.Doc, _ = goquery.NewDocument(url)
+  cp.doc, _ = goquery.NewDocument(url)
 
   return cp
 }
 
 func (cp *categoryPage)process() (zero []processor) {
-  cp.Doc.Find("div.mt-xs-2 span").Each(func(i int, s *goquery.Selection) {
+  cp.doc.Find("div.mt-xs-2 span").Each(func(i int, s *goquery.Selection) {
     match, _ := regexp.MatchString("\\([,0-9]* items\\)", s.Text())
      if (match) {
        result := strings.TrimSpace(s.Text())
@@ -46,14 +45,5 @@ func (cp *categoryPage)url() string {
 }
 
 func(c *category)write() {
-  db, err := gorm.Open("mysql", "-connection tbd")
-  db.LogMode(true)
-  if err != nil {
-    fmt.Println(err)
-  }
-  defer db.Close()
-  fmt.Println(c)
-  db.CreateTable(&category{})
-
-  // db.Create(c)
+   db.Create(c)
 }
--- etsy.go
+++ etsy.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"flag"
 )
+
+var db *gorm.DB
+
 
 type processor interface {
 	url() (string)
@@ -10,6 +16,15 @@ type processor interface {
 }
 
 func main() {
+	var err error
+	flag.Parse()
+	connection := flag.Args()[0]
+	db, err = gorm.Open("mysql", connection)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
 	entryUrl := "https://www.etsy.com/dynamic-sitemaps.xml?sitemap=browseindex"
 
 	entryPage := newSiteMapMetaPage(entryUrl);
