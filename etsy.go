330f2612044c3bc98fdd7099c627618f944a7da4
Diana Suvorova <diana.suvorova@ayasdi.com>
2017-05-01 16:06:29 -0700

    it actually needs to be a robots page

--- etsy.go
+++ etsy.go
@@ -13,17 +13,15 @@ var db *gorm.DB
 func main() {
 	var err error
 	flag.Parse()
-	connection := "boris:B@ckspace123@tcp(54.215.211.253:3306)/etsy?parseTime=true"
-	//connection := flag.Args()[0]
+	connection := flag.Args()[0]
 	db, err = gorm.Open("mysql", connection)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
 
-	// entryUrl := "https://www.etsy.com/robots.txt"
-	// entryPage := newRobotsPage(entryUrl)
+	entryUrl := "https://www.etsy.com/robots.txt"
+	entryPage := newRobotsPage(entryUrl)
 
-	entryPage := newSiteMapMetaPage("https://www.etsy.com/dynamic-sitemaps.xml?sitemap=shoplisting_index2&min=5040064&max=5050063")
 	startQueuer(entryPage)
 }
